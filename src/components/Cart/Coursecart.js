d5b48dc02e93110bca33c630d7956bcb3ddc11d1
Sayed Sajid Ali <sajidlpu1@gmail.com>
2020-05-02 16:07:47 +0530

    Web url changed

--- src/components/Cart/Coursecart.js
+++ src/components/Cart/Coursecart.js
@@ -38,7 +38,7 @@ export default class Coursecart extends Component {
                                         data-item-id={node.id}
                                         data-item-name={node.title}
                                         data-item-price={node.price}
-                                        data-item-url="https://learncodeonline.in"
+                                        data-item-url="https://digilearn.netlify.app/"
                                         data-item-image={node.image.fixed.src}>
                                             Join Now
                                         </button>
